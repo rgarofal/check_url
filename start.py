3bc16a924d806c4abc76f5498aa2d762bdb4f5bd
roberto <roby934@ymail.com>
2019-07-08 22:35:05 +0200

    prima versione

---
+++ start.py
@@ -0,0 +1,44 @@
+import json
+import concurrent.futures
+import urllib.request
+import urllib.error
+
+URLS = ['http://www.foxnews.com/',
+        'http://www.cnn.com/',
+        'http://europe.wsj.com/',
+        'http://www.bbc.co.uk/',
+        'http://some-made-up-domain.com/',
+        'https://httpstat.us/400']
+
+# Retrieve a single page and report the URL and contents
+def load_url(url, timeout):
+    with urllib.request.urlopen(url, timeout=timeout) as conn:
+        return conn.read()
+
+def open_url(url, timeout):
+
+       try:
+           with urllib.request.urlopen(url, timeout=timeout) as conn:
+            data = conn.read()
+
+       except urllib.error.HTTPError as e:
+            print(e.code)
+            print(e.read())
+            data = e.read()
+       return data
+# We can use a with statement to ensure threads are cleaned up promptly
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    # Start the load operations and mark each future with its URL
+    future_to_url = {executor.submit(load_url, url, 60): url for url in URLS}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            data = future.result()
+        except urllib.error.HTTPError as e:
+            print(e.code)
+            print(e.read())
+            
+        except Exception as exc:
+            print('%r generated an exception: %s' % (url, exc))
+        else:
+            print('%r page is %d bytes' % (url, len(data)))
